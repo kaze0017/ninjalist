bb078aaafd4525c66be409f513ecdb54fa22b712
Keivan Kazemi <kaze0017@algonquinlive.com>
2023-06-05 21:21:21 -0400

    initial

---
+++ components/Footer.js
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <footer>Copyright 2023 Ninja</footer>;
+};
+
+export default Footer;
---
+++ pages/about.js
@@ -0,0 +1,22 @@
+import Head from "next/head";
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="Keywords" conntent="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas
+          consequatur, placeat accusantium quae tenetur commodi eos harum,
+          eligendi recusandae tempore porro! Dolor blanditiis fuga aperiam nam,
+          illum vitae cupiditate libero.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
